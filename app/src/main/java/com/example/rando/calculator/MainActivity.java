d4b76f5bc46870e16dde2b4b54eee5758392719b
andonaegui <andonaegui@gmail.com>
2016-08-12 00:22:53 -0400

    First Commit :D

---
+++ app/src/main/java/com/example/rando/calculator/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.rando.calculator;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button one;
+    private Button two;
+    private Button three;
+    private Button four;
+    private Button five;
+    private Button six;
+    private Button seven;
+    private Button eight;
+    private Button nine;
+    private Button plus;
+    private Button minus;
+    private Button equals;
+    private Context appCtx;
+    private String operator;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startComponents();
+        startEvents();
+    }
+
+
+
+    private void startComponents() {
+        appCtx = getApplicationContext();
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four= (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six= (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight= (Button) findViewById(R.id.eight);
+        nine= (Button) findViewById(R.id.nine);
+        plus= (Button) findViewById(R.id.plus);
+        minus= (Button) findViewById(R.id.minus);
+        equals= (Button) findViewById(R.id.equals);
+    }
+
+    private void startEvents() {
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operator = "minus";
+            }
+        });
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operator = "plus";
+            }
+        });
+
+        equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resolve();
+            }
+        });
+    }
+
+
+    private void resolve() {
+
+    }
+}
